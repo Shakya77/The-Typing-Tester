b9155dcb89426c99672dffbc3bab789a064aeec0
shakya77 <bijanshakya145@gmail.com>
2025-03-30 19:13:44 +0545

    made header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-
+      <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 
 function Header() {
     return (
         <div>
-
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Clone Monkeytype</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
